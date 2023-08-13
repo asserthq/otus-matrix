04ee1accbf18a832ecd3d25141d74d72b1a8cfae
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-08-13 21:17:03 +0700

    add some code for test dbg

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include "sparse_matrix.h"
+#include <iostream>
+
+int fact(int n)
+{
+    if(n < 2) return 1;
+    return n * fact(n-1);
+}
 
 int main()
 {
-	
+    auto a = fact(5);
+	std::cout << a << '\n';
 	
     return 0;
 }
